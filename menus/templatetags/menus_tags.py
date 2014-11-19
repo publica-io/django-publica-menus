ced4bcccd1bad855c17a988eb569bb58ad1e935a
harshad bhatia <harshadbhatia@harshads-MacBook-Pro.local>
2014-11-19 21:09:33 +1100

    Added extra template tag to return parent child links

--- menus/templatetags/menus_tags.py
+++ menus/templatetags/menus_tags.py
@@ -1,16 +1,74 @@
-from django import template
-from menus.models import Menu
+from django.template import Library, loader, Context
+from django.template import RequestContext
 
-register = template.Library()
+from menus.models import Menu, MenuItem
 
-@register.inclusion_tag('menus/menu_list.html')
-def menu(slug, *args, **kwargs):
+register = Library()
+
+@register.simple_tag(takes_context=True)
+def menu(context, slug, template='menus/menu_list.html', *args, **kwargs):
     try:
-        menu = Menu.objects.get(
+        menu = Menu.objects.prefetch_related('items').get(
             enabled=True,
             slug=slug
         )
     except Menu.DoesNotExist, Menu.MultipleObjectsReturned:
         menu = None
 
-    return menu
+    t = loader.get_template(template)
+    return t.render(Context({
+        'menu': menu,
+        'request': context['request']
+    }))
+
+
+@register.simple_tag(takes_context=True)
+def get_active_parent_or_child(context, template, parent=True):
+    """
+        The code would do the following:
+        given the url get active link, if it is parent link then return all parents
+        if child then return parent with all its children
+
+        Takes input if all parent links are wanted all or all children links are wanted
+    """
+
+    current_url = context['request'].path
+    active_child_url = None
+    active_parent_url = None
+    menu = ''
+
+    try:
+        menu_item = MenuItem.objects.get(
+            link__url=current_url,
+        )
+
+    except MenuItem.DoesNotExist, MenuItem.MultipleObjectsReturned:
+        menu_item = None
+    # Well get the menu either way
+    try:
+        menu = Menu.objects.prefetch_related('items').get(
+            enabled=True,
+            slug=menu_item.menu.slug
+        )
+    except Menu.DoesNotExist, Menu.MultipleObjectsReturned:
+        menu = None
+
+    # If the link is child
+    if menu_item and menu_item.parent is not None:
+        active_child_url = menu_item.link.get_url()
+        active_parent_url = menu_item.parent.link.get_url()
+        menu_item = menu_item.parent.children.all()
+    else:
+        active_parent_url = menu_item.link.get_url()
+        menu_item = menu_item.children.all()
+
+
+    t = loader.get_template(template)
+    return t.render(Context({
+        'menu': menu,
+        'menu_item': menu_item,
+        'active_child_url': active_child_url,
+        'active_parent_url': active_parent_url,
+        'request': context['request'],
+        'parent': parent
+    }))
